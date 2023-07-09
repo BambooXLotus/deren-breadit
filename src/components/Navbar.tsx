b5ce0de4d73d9f90c459036625e2104f0940fd00
Deren Somsanith <derensomsanith@gmail.com>
2023-07-09 13:53:00 -0500

    added login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,32 @@
-import Link from 'next/link'
-import { Icons } from './Icons'
-import { buttonVariants } from './ui/Button'
+import Link from "next/link";
+import { Icons } from "./Icons";
+import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
 
-type NavbarProps = {
-  id?: string
-}
+export const Navbar = async () => {
+  const session = await getAuthSession();
 
-export const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-zinc-300 z-[10] py-2">
-      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+    <div className="fixed inset-x-0 top-0 z-[10] h-fit border-zinc-300 bg-zinc-100 py-2">
+      <div className="container mx-auto flex h-full max-w-7xl items-center justify-between gap-2">
         {/* LOGO */}
-        <Link href="/" className="flex gap-2 items-center">
+        <Link href="/" className="flex items-center gap-2">
           <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
-          <p className="hidden text-zinc-700 text-sm font-medium md:block">
+          <p className="hidden text-sm font-medium text-zinc-700 md:block">
             Deren Breadit
           </p>
         </Link>
 
         {/* SEARCH */}
 
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign In
-        </Link>
+        {session ? (
+          <p>Logged in</p>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
