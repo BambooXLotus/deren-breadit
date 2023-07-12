2f09a9e8b6fd3f177a9a9a9001aae25ff35cbdf9
Deren Somsanith <derensomsanith@gmail.com>
2023-07-11 19:17:09 -0500

    starting the subreddit page

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,4 +1,8 @@
+import { MiniCreatePost } from "@/components/MiniCreatePost";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
 
 type PageProps = {
   params: {
@@ -10,5 +14,29 @@ export default async function SubredditPage({ params }: PageProps) {
   const { slug } = params;
   const session = await getAuthSession();
 
-  return <div>Page</div>;
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+        take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+      },
+    },
+  });
+
+  if (!subreddit) return notFound();
+
+  return (
+    <>
+      <h1 className="font-bold text-3xl md:text-4xl h-14">
+        r/{subreddit.name}
+      </h1>
+      <MiniCreatePost session={session} />
+    </>
+  );
 }
