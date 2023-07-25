7bbd11d5737c117fa3c2ba5a5575c65bc437f0c8
Deren Somsanith <derensomsanith@gmail.com>
2023-07-25 17:30:28 -0500

    added the post feed

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
 type ProvidersProps = {
   children: React.ReactNode;
@@ -10,6 +11,8 @@ export const Providers: React.FC<ProvidersProps> = ({ children }) => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>{children}</SessionProvider>
+    </QueryClientProvider>
   );
 };
