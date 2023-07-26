ba75d4eb12947f8a86352d2b19983e55ab59684c
Deren Somsanith <derensomsanith@gmail.com>
2023-07-26 16:40:56 -0500

    added upvote and downvote feature

---
+++ src/app/api/subreddit/post/vote/route.ts
@@ -0,0 +1,125 @@
+import { getAuthSession } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { redis } from '@/lib/redis';
+import { PostVoteValidator } from '@/lib/validators/vote';
+import { CachedPost } from '@/types/redis';
+import { z } from 'zod';
+
+const CACHE_AFTER_UPVOTES = 1
+
+export async function PATCH(req: Request) {
+  try {
+    const session = await getAuthSession()
+
+    if (!session?.user) {
+      return new Response('UNAUTHORIZED', { status: 401 })
+    }
+
+    const body = await req.json()
+    const { postId, voteType } = PostVoteValidator.parse(body)
+
+    const existingVote = await db.vote.findFirst({
+      where: {
+        userId: session.user.id,
+        postId
+      }
+    })
+
+    const existingPost = await db.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        author: true,
+        votes: true
+      }
+    })
+
+    if (!existingPost) {
+      return new Response('Not found', { status: 404 })
+    }
+
+    if (existingVote) {
+      if (existingVote.type === voteType) {
+        await db.vote.delete({
+          where: {
+            userId_postId: {
+              postId,
+              userId: session.user.id
+            }
+          }
+        })
+        return new Response("OK")
+      }
+
+      await db.vote.update({
+        where: {
+          userId_postId: {
+            postId,
+            userId: session.user.id
+          },
+        },
+        data: {
+          type: voteType
+        }
+      })
+
+      // recount the votes
+      const votesCnt = existingPost.votes.reduce((acc, vote) => {
+        if (vote.type === 'UP') return acc + 1
+        if (vote.type === 'DOWN') return acc - 1
+        return acc
+      }, 0)
+
+      if (votesCnt >= CACHE_AFTER_UPVOTES) {
+        const cachedPost: CachedPost = {
+          id: existingPost.id,
+          authorUsername: existingPost.author.username ?? '',
+          content: JSON.stringify(existingPost.content),
+          title: existingPost.title,
+          currentVote: voteType,
+          createdAt: existingPost.createdAt
+        }
+
+        await redis.hset(`post:${postId}`, cachedPost)
+      }
+
+      return new Response('OK')
+    }
+
+    await db.vote.create({
+      data: {
+        type: voteType,
+        userId: session.user.id,
+        postId,
+      }
+    })
+
+    // recount the votes
+    const votesCnt = existingPost.votes.reduce((acc, vote) => {
+      if (vote.type === 'UP') return acc + 1
+      if (vote.type === 'DOWN') return acc - 1
+      return acc
+    }, 0)
+
+    if (votesCnt >= CACHE_AFTER_UPVOTES) {
+      const cachedPost: CachedPost = {
+        id: existingPost.id,
+        authorUsername: existingPost.author.username ?? '',
+        content: JSON.stringify(existingPost.content),
+        title: existingPost.title,
+        currentVote: voteType,
+        createdAt: existingPost.createdAt
+      }
+
+      await redis.hset(`post:${postId}`, cachedPost)
+    }
+    return new Response('OK')
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(error.message, { status: 422 })
+    }
+
+    return new Response('Could not vote', { status: 500 })
+  }
+}
--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useCustomToast } from "@/hooks/use-custom-toast";
+import { toast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
+import { PostVoteRequest } from "@/lib/validators/vote";
 import { usePrevious } from "@mantine/hooks";
 import { VoteType } from "@prisma/client";
-import { useEffect, useState } from "react";
-import { Button } from "../ui/Button";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
 import { ArrowBigDown, ArrowBigUp } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
+import { Button } from "../ui/Button";
 
 type PostVoteClientProps = {
   postId: string;
@@ -28,9 +33,64 @@ export const PostVoteClient: React.FC<PostVoteClientProps> = ({
     setCurrentVote(initialVote);
   }, [initialVote]);
 
+  const { mutate: vote } = useMutation({
+    mutationFn: async (voteType: VoteType) => {
+      const payload: PostVoteRequest = {
+        postId,
+        voteType,
+      };
+
+      await axios.patch("/api/subreddit/post/vote", payload);
+    },
+    onMutate: (voteType: VoteType) => {
+      // optimistic render
+      if (currentVote === voteType) {
+        // User is voting the same way again, so remove their vote
+        setCurrentVote(undefined);
+        if (voteType === "UP") setVotesCnt((prev) => prev - 1);
+        else if (voteType === "DOWN") setVotesCnt((prev) => prev + 1);
+      } else {
+        // User is voting in the opposite direction, so subtract 2
+        setCurrentVote(voteType);
+        if (voteType === "UP")
+          setVotesCnt((prev) => prev + (currentVote ? 2 : 1));
+        else if (voteType === "DOWN")
+          setVotesCnt((prev) => prev - (currentVote ? 2 : 1));
+      }
+    },
+    onError: (err, voteType) => {
+      // optimistic render reset
+      if (voteType === "UP") {
+        setVotesCnt((prev) => prev - 1);
+      } else {
+        setVotesCnt((prev) => prev + 1);
+      }
+
+      // reset current vote
+      setCurrentVote(prevVote);
+
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: "SOMETHING WONG!",
+        description: "SOMETHING WONG!",
+        variant: "destructive",
+      });
+    },
+  });
+
   return (
     <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-3 sm:pb-0">
-      <Button size="sm" variant="ghost" aria-label="upvote">
+      <Button
+        size="sm"
+        variant="ghost"
+        aria-label="upvote"
+        onClick={() => vote("UP")}
+      >
         <ArrowBigUp
           className={cn("h-5 w-5 text-zinc-700", {
             "text-emerald-500 fill-emerald-500": currentVote === "UP",
@@ -42,7 +102,12 @@ export const PostVoteClient: React.FC<PostVoteClientProps> = ({
         {votesCnt}
       </p>
 
-      <Button size="sm" variant="ghost" aria-label="downvote">
+      <Button
+        size="sm"
+        variant="ghost"
+        aria-label="downvote"
+        onClick={() => vote("DOWN")}
+      >
         <ArrowBigDown
           className={cn("h-5 w-5 text-zinc-700", {
             "text-red-500 fill-red-500": currentVote === "DOWN",
